e0210970893a1ee38d9ddc218db80935369f1b2d
Gaia Belli <gaiabelli0@gmail.com>
2022-04-10 15:45:32 +0200

    Aggiunto codice versione programma

---
+++ version.h
@@ -0,0 +1,12 @@
+//
+// Created by gaiab on 10/04/2022.
+//
+
+#ifndef BITMAPIMAGE_CPP_VERSION_H
+#define BITMAPIMAGE_CPP_VERSION_H
+
+#include <string>
+
+static const std::string VERSION = "V1.0";
+
+#endif //BITMAPIMAGE_CPP_VERSION_H
